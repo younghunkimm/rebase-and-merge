281ae24b09c4d6e7c2c49e25477d9c9bf2f521ba
김영훈 <hunifive@gmail.com>
2025-08-07 21:24:20 +0900

    init

---
+++ src/main/java/com/example/rebaseandmerge/RebaseAndMergeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rebaseandmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebaseAndMergeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebaseAndMergeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rebaseandmerge/RebaseAndMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rebaseandmerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaseAndMergeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
